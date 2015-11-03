94f56be930c44ddc5fc0288649ac8ebb89b6502e
Luka Vilaj <lvilaj@gsma.com>
2015-11-03 02:49:50 +0000

    Files updated

--- marketintel.php
+++ marketintel.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
+<head>
+  <meta charset="utf-8" />
+  <title>MarketIntel Data</title>
+</head>
 <html>
 <body>
 <?php
+header("Content-Type: application/json");
+header("Cache-Control: no-cache");
 $homepage = file_get_contents('https://tools.ayima.com/techtest/api/marketintel');
 echo $homepage;
 ?>
